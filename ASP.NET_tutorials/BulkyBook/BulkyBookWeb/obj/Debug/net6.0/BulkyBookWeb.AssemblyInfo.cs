806da0344a47d420f9e678e7c4e9cc3c2a51e9bf
MaryPeteva <shinosedai404@gmail.com>
2024-01-13 20:10:57 +0200

    Only Tools, just page designs

--- ASP.NET_tutorials/BulkyBook/BulkyBookWeb/obj/Debug/net6.0/BulkyBookWeb.AssemblyInfo.cs
+++ ASP.NET_tutorials/BulkyBook/BulkyBookWeb/obj/Debug/net6.0/BulkyBookWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyBookWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+925d076a56c1221e9f29ddf5686144b2d2e08327")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cd8c23c91dd52026a1a00e5b9fe4822b870190e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyBookWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyBookWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
