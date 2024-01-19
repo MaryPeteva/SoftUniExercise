cc5266836c88227ed1611bc27fcb42f3c75226df
MaryPeteva <shinosedai404@gmail.com>
2024-01-19 08:56:28 +0200

    moved onlyTools to it's own repo

--- FinalProject/OnlyTools/OnlyToolsWeb/obj/Release/net6.0/OnlyToolsWeb.AssemblyInfo.cs
+++ FinalProject/OnlyTools/OnlyToolsWeb/obj/Release/net6.0/OnlyToolsWeb.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlyToolsWeb")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d5e33a5e08e193a3c317d58a20983ba40ab60b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlyToolsWeb")]
